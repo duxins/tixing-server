c369fe0c1cd9848be6aafb9b47519485790d5bd0
duxin <duxins@gmail.com>
2014-10-15 18:40:48 +0800

    Add apn_workder

---
+++ app/workers/apn_worker.rb
@@ -0,0 +1,21 @@
+require 'apn_connection'
+
+class APNWorker
+  include Sidekiq::Worker
+
+  APN_POOL = ConnectionPool.new(:size => 2, :timeout => 300) do
+    APNConnection.new
+  end
+
+  def perform(message, token, custom_data = nil)
+
+    APN_POOL.with do |connection|
+        notification = Houston::Notification.new(device: token)
+        notification.alert = message
+        notification.sound = 'default'
+        notification.custom_data = custom_data
+        connection.write(notification.message)
+    end
+  end
+
+end
