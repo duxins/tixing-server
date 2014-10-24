e27c4cecd2eeac5022d70c071f9e75405d0dac8e
duxin <duxins@gmail.com>
2014-10-24 17:23:48 +0800

    Generate absolute url for service icon

--- app/api/tixing/entities/service.rb
+++ app/api/tixing/entities/service.rb
@@ -3,11 +3,12 @@ module Tixing
     class Service < Grape::Entity
       expose :id
       expose :name
-      expose :icon
+      expose :icon do |service, options|
+        "#{options[:env]['rack.url_scheme']}://#{options[:env]['HTTP_HOST']}/icons/#{service.icon}" if service.icon
+      end
       expose :url do |service, options|
         "#{options[:env]['rack.url_scheme']}://#{options[:env]['HTTP_HOST']}#{service.url}"
       end
-
     end
   end
 end
