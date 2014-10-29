b948a5c77731fdf8f1c6ec57c331fc8b54c8c9d5
duxin <duxins@gmail.com>
2014-10-29 13:05:22 +0800

    Convert UTC timestamp to ISO 8601

--- app/api/api_v1/entities/device.rb
+++ app/api/api_v1/entities/device.rb
@@ -1,10 +1,11 @@
 module APIv1
   module Entities
     class Device < Grape::Entity
+      format_with(:iso_timestamp) { |dt| dt.iso8601 }
       expose :id
       expose :name
-      expose :created_at
-      expose :updated_at
+      expose :created_at, format_with: :iso_timestamp
+      expose :updated_at, format_with: :iso_timestamp
     end
   end
 end
--- app/api/api_v1/entities/notification.rb
+++ app/api/api_v1/entities/notification.rb
@@ -1,9 +1,14 @@
 module APIv1
   module Entities
     class Notification < Grape::Entity
+      format_with(:iso_timestamp) { |dt| dt.iso8601 }
+
       expose :id
       expose :message
       expose :service, with: APIv1::Entities::Service
+
+      expose :created_at, format_with: :iso_timestamp
+
     end
   end
 end
