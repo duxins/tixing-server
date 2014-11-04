725445b195be6ffe91090b98d7c0c1a9694fb8c7
duxin <duxins@gmail.com>
2014-11-04 16:29:32 +0800

    Add deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,61 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rvm'
+require 'mina_sidekiq/tasks'
+require 'mina/whenever'
+
+set :domain, 'web.tixing'
+set :user,   'deploy'
+set :deploy_to, '/var/www/tixing'
+set :repository, 'git@github.com:duxins/tixing-server.git'
+set :branch, 'master'
+
+set :shared_paths, [
+    'config/database.yml',
+    'config/application.yml',
+    'tmp',
+    'log',
+]
+
+task :environment do
+   invoke :'rvm:use[ruby-2.1.4]'
+end
+
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/pids"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/application.yml"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :'sidekiq:quiet'
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      invoke :'whenever:update'
+      invoke :'sidekiq:restart'
+    end
+  end
+end
+
