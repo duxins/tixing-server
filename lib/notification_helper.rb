e8de31ecde344c2e18ce7961cbfffe3e45d6e8f0
duxin <duxins@gmail.com>
2014-11-12 19:02:56 +0800

    Use rpush instead of houston in NotificationHelper

--- lib/notification_helper.rb
+++ lib/notification_helper.rb
@@ -26,8 +26,30 @@ class NotificationHelper
                 user.sound
               end
 
-      APNWorker.perform_async(push_message, device['token'], sound, {id: notification.id})
+      self.deliver(push_message, device['token'], sound, {id: notification.id})
     end
 
+  end
+
+  def self.deliver(message, token, sound, custom_data = {})
+    cache_key = "device:token:#{token}"
+
+    if Rails.cache.read(cache_key)
+      sound = nil
+    else
+      Rails.cache.write(cache_key, true, expires_in: 30.seconds)
+    end
+
+    sound = "#{sound}.caf" if sound && sound != 'default'
+
+    Rails.logger.info "[APN] sent to #{token[0..10]}, message: #{message[0..10]}.., sound: #{sound}, data: #{custom_data}"
+
+    n = Rpush::Apns::Notification.new
+    n.app = Rpush::Apns::App.find_by_name('ios')
+    n.device_token = token
+    n.alert = message
+    n.sound = sound if sound
+    n.data = custom_data
+    n.save!
   end
 end
