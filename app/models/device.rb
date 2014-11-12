3c16a241444b84ad42b8f42c0ec4a555e7711d5d
duxin <duxins@gmail.com>
2014-11-12 13:57:16 +0800

    Add device token validation

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,7 @@
 class Device < ActiveRecord::Base
   belongs_to :user
 
+  validates :token, format: { with: /\A[0-9a-z]{64}\z/i}
   validates :timezone, inclusion: { in: -14..14 }
 
   def at_night?
