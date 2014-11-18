23fa00c23911b2111ae21babd3ad9894f3941fab
duxin <duxins@gmail.com>
2014-11-18 16:45:45 +0800

    Add unit tests for `V2ex::Monitoring` model

---
+++ test/models/netease/monitoring_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+
+class Netease::MonitoringTest < ActiveSupport::TestCase
+  def setup
+  end
+
+  test 'should match' do
+    news = {
+        'title' => '习近平将走遍澳大利亚所有州 幽默要证书',
+        'digest' => '中国国家主席习近平在堪培拉会见澳大利亚总督科斯格罗夫'
+    }
+
+    assert_not create_monitoring('包子').matched?(news)
+    assert_not create_monitoring('习 近平').matched?(news)
+
+    assert create_monitoring('习近平').matched?(news)
+    assert create_monitoring('堪培拉').matched?(news)
+
+    news = {
+        'title' => '苹果推出iOS 8.1.1升级包 改善旧设备体验',
+        'digest' => '11月18日消息，苹果公司已对上个月针对iPhone、iPad和iPod  Touch发布的iOS 8.1操作系统正式推出了iOS 8.1.1升级补丁'
+    }
+
+    assert create_monitoring('ios').matched?(news)
+    assert create_monitoring('iphone').matched?(news)
+    assert create_monitoring('IPAD').matched?(news)
+    assert create_monitoring('iPod Touch').matched?(news)
+
+  end
+
+  test 'should format keyword properly' do
+    assert_equal '关键词', create_monitoring('关键词').keyword
+    assert_equal '关键词', create_monitoring('   关键词   ').keyword
+    assert_equal '关 键 词', create_monitoring('关    键  词').keyword
+    assert_equal 'keyword', create_monitoring('Keyword').keyword
+  end
+
+  test 'should not save with invalid keyword' do
+    assert_not Netease::Monitoring.new(keyword: '').save
+    assert_not Netease::Monitoring.new(keyword: 'a').save
+    assert_not Netease::Monitoring.new(keyword: '啊').save
+    assert_not Netease::Monitoring.new(keyword: ' a ').save
+  end
+
+  test 'should not save when keyword exists' do
+    Netease::Monitoring.create(user_id: 1, keyword: '关键词')
+
+    assert_not Netease::Monitoring.new(user_id: 1, keyword: '关键词').save
+    assert Netease::Monitoring.new(user_id:2, keyword: '关键词').save
+  end
+
+private
+  def create_monitoring(keyword)
+    Netease::Monitoring.create(keyword: keyword)
+  end
+end
