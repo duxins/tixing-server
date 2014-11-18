82d004fdc5430dfe175bb1355e4dbf3f4b87a466
duxin <duxins@gmail.com>
2014-11-18 18:13:34 +0800

    Add `Netease` module and unit tests

--- app/models/netease.rb
+++ app/models/netease.rb
@@ -1,8 +1,88 @@
 module Netease
+
   SERVICE_ID = 3
 
-  def self.table_name_prefix
-    'netease_'
+  CATEGORIES = {
+    high: {
+      '头条' => 'T1348647853363',
+      '娱乐' => 'T1348648517839',
+      '体育' => 'T1348649079062',
+    },
+    medium: {
+      '财经' => 'T1348648756099',
+      '科技' => 'T1348649580692',
+    }
+  }
+
+  class << self
+
+    def table_name_prefix
+      'netease_'
+    end
+
+    def recent (options = {})
+      priority = options[:priority] || :high
+      list = []
+
+      if priority == :all
+        categories = CATEGORIES[:high].merge(CATEGORIES[:medium])
+      else
+        categories = CATEGORIES[priority]
+      end
+
+      categories.each do |k, v|
+        list.concat(self.fetch_news_list(v))
+      end
+
+      list
+    end
+
+    def fetch_news_list(cid)
+      url = "http://c.3g.163.com/nc/article/list/#{cid}/0-20.html"
+
+      begin
+
+        c = Curl::Easy.perform(url) do |curl|
+          curl.headers['User-Agent'] = 'tixing'
+          curl.connect_timeout = 5
+        end
+
+        json = JSON.parse(c.body_str)
+
+        raise "#{c.body_str[0..30]}" unless json.has_key?(cid)
+
+        collection = json[cid]
+
+        collection.sort_by! {|news|DateTime.parse(news['ptime'])}.reverse!
+
+        collection.select! do |news|
+          time_diff = DateTime.now.to_i - DateTime.parse(news['ptime'] + "+8").to_i
+          # 只保留1小时内发布的新闻
+          time_diff < 3600 and news['url'].present? and not Rails.cache.read(self.cache_key news['docid'])
+        end
+
+        collection.map! do |news|
+          Rails.cache.write(self.cache_key(news['docid']), true, expires_in: 2.hour)
+
+          news['published_at'] = DateTime.parse(news['ptime'] + "+8")
+          news['url'] = news['url_3w']
+          news['img'] = news['img_src']
+
+          news.select {|k, v| %w[docid title digest published_at url img].include? k }
+        end
+
+        collection
+      rescue => e
+        Rails.logger.error("netease API error: #{url}, #{e.message}")
+        []
+      end
+
+    end
+
+    def cache_key (id)
+      "netease:news:doc:#{id}"
+    end
+
   end
 
 end
---
+++ test/models/netease/netease_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class NeteaseTest < ActiveSupport::TestCase
+  def setup
+    @freeze_time = DateTime.new(2014, 11, 18, 17, 50, 0 ,'+8')
+    @news_count_high = 4
+    @news_count_medium = 2
+  end
+
+  test 'service_id should equal 3' do
+    assert_equal 3, Netease::SERVICE_ID
+  end
+
+  test 'categories should have two priorities' do
+    assert Netease::CATEGORIES.has_key? :high
+    assert Netease::CATEGORIES.has_key? :medium
+  end
+
+  test 'should return recent news' do
+    Rails.cache.clear
+
+    Timecop.freeze(@freeze_time) do
+      VCR.use_cassette('netease_recent_news_high') do
+        news = Netease.recent(priority: :high)
+        assert_equal @news_count_high, news.count
+
+        news.each do |item|
+          assert item.has_key?('title')
+          assert item.has_key?('digest')
+          assert item.has_key?('url')
+          assert item.has_key?('img')
+          assert item.has_key?('published_at') and item['published_at'].is_a? DateTime
+        end
+      end
+
+      VCR.use_cassette('netease_recent_news_medium') do
+        news = Netease.recent(priority: :medium)
+        assert_equal @news_count_medium, news.count
+      end
+    end
+
+  end
+
+  test 'should return empty array when error occurs' do
+    stub_request(:any, /163.com/).to_timeout
+    assert_empty Netease.recent
+  end
+
+  test 'should skip captured news' do
+    Rails.cache.clear
+    Timecop.freeze(@freeze_time) do
+      VCR.use_cassette('netease_recent_news_high') do
+        news = Netease.recent(priority: :high)
+        assert_equal @news_count_high, news.count
+      end
+
+      VCR.use_cassette('netease_recent_news_high') do
+        news = Netease.recent(priority: :high)
+        assert_equal 0, news.count
+      end
+    end
+  end
+
+end
