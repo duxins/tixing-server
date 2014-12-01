087e3b8f98a8a02e2d5ad347f7ed4eae6ddfcf63
duxin <duxins@gmail.com>
2014-12-01 19:31:41 +0800

    Add unit test for Jingdong::Price

--- app/models/jingdong/price.rb
+++ app/models/jingdong/price.rb
@@ -1,3 +1,4 @@
 class Jingdong::Price < ActiveRecord::Base
   belongs_to :product, class_name: 'Jingdong::Product'
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
--- test/models/jingdong/price_test.rb
+++ test/models/jingdong/price_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class Jingdong::PriceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save with invalid price' do
+    assert Jingdong::Price.new(price:1).save
+    assert_not Jingdong::Price.new(price:0).save
+    assert_not Jingdong::Price.new(price:-1.0).save
+    assert_not Jingdong::Price.new(price:-10000000).save
+  end
 end
