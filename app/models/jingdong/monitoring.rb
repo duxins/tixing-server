911cb6e64c4d3d9c979129d03295489fff163219
duxin <duxins@gmail.com>
2014-12-01 19:32:50 +0800

    Add unit test for Jingdong::Monitoring

--- app/models/jingdong/monitoring.rb
+++ app/models/jingdong/monitoring.rb
@@ -1,4 +1,11 @@
 class Jingdong::Monitoring < ActiveRecord::Base
   belongs_to :product, class_name: 'Jingdong::Product', counter_cache: true
   belongs_to :user, class_name: '::User'
+
+  validates :threshold, presence:true, numericality: { greater_than: 0}
+
+  validate on: :create do
+    errors.add(:base, :too_many, limit: 10) if Jingdong::Monitoring.where(user_id: self.user_id).count >= 10
+  end
+
 end
--- test/models/jingdong/monitoring_test.rb
+++ test/models/jingdong/monitoring_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class Jingdong::MonitoringTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not exceed products limit (10)' do
+    I18n.locale = :en
+    10.times do
+      assert Jingdong::Monitoring.new(user_id:2, threshold: 1).save
+    end
+    monitoring =  Jingdong::Monitoring.create(user_id:2, threshold: 1)
+    assert_not monitoring.id
+    error_message = I18n.t('activerecord.errors.models.jingdong/monitoring.attributes.base.too_many', limit: 10)
+    assert_not error_message.match(/^translation missing/)
+    assert_equal error_message, monitoring.errors.full_messages.first
+  end
+
+  test 'should not save with invalid threshold' do
+    assert Jingdong::Monitoring.new(threshold: 1).save
+    assert_not Jingdong::Monitoring.new(threshold: -1).save
+    assert_not Jingdong::Monitoring.new(threshold: 0).save
+  end
 end
