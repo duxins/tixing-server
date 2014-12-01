47f86e8c0332a2d4f467f700b6c81df6f123a6c5
duxin <duxins@gmail.com>
2014-12-01 19:34:34 +0800

    Add Jingdong::Product model and unit tests

--- app/models/jingdong/product.rb
+++ app/models/jingdong/product.rb
@@ -1,3 +1,54 @@
 class Jingdong::Product < ActiveRecord::Base
-end
+  has_many :prices, class_name: 'Jingdong::Price'
+  has_many :monitorings, :class_name => 'Jingdong::Monitoring'
 
+  def self.fetch(id)
+    product = self.find_by_id(id)
+    return product if product
+    url = "http://item.jd.com/#{id}.html"
+    Rails.logger.info("[JD] Request API: #{url}")
+    begin
+      c = Timeout::timeout(5) do
+        Curl::Easy.perform(url) do |curl|
+          curl.headers['User-Agent'] = 'tixing'
+          curl.headers["Accept-Encoding"] = "gzip,deflate"
+          curl.connect_timeout = 5
+        end
+      end
+
+      html = c.body_str
+      html = ActiveSupport::Gzip.decompress(html) if c.header_str =~ /Content-Encoding: gzip/
+
+      html.force_encoding('gbk')
+      html.encode!('utf-8')
+
+      name = html.match(%r{<h1>([^<]*)</h1>}) {|m| m[1]}
+      image = html.match(%r{src: '([^']*)'}) {|m| m[1]}
+      price = self.fetch_price(id)
+
+      self.create(id:id, name: name, price:price, image:image)
+    rescue => e
+      Rails.logger.error("[JD] #{e.message}")
+      nil
+    end
+  end
+
+  def self.fetch_price(id)
+    url = "http://p.3.cn/prices/mgets?type=1&skuIds=J_#{id}"
+    json = Timeout::timeout(5) do
+      Curl::Easy.perform(url) do |curl|
+        curl.headers['User-Agent'] = 'tixing'
+      end.body_str
+    end
+    prices = JSON.parse(json)
+    price = prices.first['p'].to_f
+    raise 'invalid price' if price <= 0
+    price
+  end
+
+  def image
+    image = read_attribute(:image)
+    return nil if image.nil?
+    'http://img12.360buyimg.com/n7/' + image
+  end
+end
--- test/models/jingdong/product_test.rb
+++ test/models/jingdong/product_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class Jingdong::ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product = jingdong_products(:iphone)
+  end
+
+  test 'should return absolute URL to product image' do
+    assert_match %r[^http://], @product.image
+  end
+
+  test 'should fetch product correctly' do
+    VCR.use_cassette('jingdong_product_iphone_1023438') do
+      product = Jingdong::Product.fetch(1023438)
+      assert_equal 1023438, product.id
+      assert_match '苹果（APPLE）iPhone 5s 16G版 4G手机（深空灰色）TD-LTE/TD-SCDMA/GSM', product.name
+      assert_equal 4188, product.price
+    end
+  end
+
+  test 'should fetch correct price' do
+    VCR.use_cassette('jingdong_price_iphone_1023437') do
+      assert_equal 4188, Jingdong::Product.fetch_price(1023437)
+    end
+  end
+
+  test 'should return nil when there is a network problem' do
+    stub_request(:any, /jd.com/).to_timeout
+    product = Jingdong::Product.fetch(1023438)
+    assert_not product
+  end
+
+  test 'should return cached product when there is a network problem' do
+    stub_request(:any, /jd.com/).to_timeout
+    product = Jingdong::Product.fetch(@product.id)
+    assert @product.id, product.id
+  end
 end
