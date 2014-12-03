21570e3523437291b364dace638c5fcb28cf3d8e
duxin <duxins@gmail.com>
2014-12-03 19:19:55 +0800

    Add ShunfengWorker

--- app/workers/jingdong_worker.rb
+++ app/workers/jingdong_worker.rb
@@ -9,27 +9,26 @@ class JingdongWorker
     Rails.logger.info "[JD] #{product.id} - Started Fetch Product"
     current_price = Jingdong::Product.fetch_price(id)
     Rails.logger.info "[JD] #{product.id} - Fetched Price: #{current_price}"
-    if current_price != product.price
-      Rails.logger.info "[JD] #{product.id} - Price Updates Found: #{product.price} -> #{current_price}"
+    return unless (current_price - product.price).abs > 0.01
 
-      product.update(price: current_price)
-      Jingdong::Price.create(product: product, price: current_price)
+    Rails.logger.info "[JD] #{product.id} - Price Updates Found: #{product.price} -> #{current_price}"
+    product.update(price: current_price)
+    Jingdong::Price.create(product: product, price: current_price)
 
-      monitorings = Jingdong::Monitoring.includes(:user).where(product: product, disabled: false).where('? <= threshold', current_price)
-      monitorings.each do |monitoring|
-        Rails.logger.info "[JD] #{product.id} - Notify User: #{monitoring.user.id} (threshold: #{monitoring.threshold})"
-        NotificationHelper.send ({
-          user_id: monitoring.user.id,
-          service_id: Jingdong::SERVICE_ID,
-          title: '京东商城',
-          message: "#{product.name}\n当前价格: ¥#{current_price}",
-          push_message: "京东商城:您关注的商品 '#{product.name}' 价格降到 ¥#{current_price} 了 !",
-          url: product.url,
-          web_url: product.web_url,
-          thumb: product.image,
-        })
-        monitoring.update(disabled: true)
-      end
+    monitorings = Jingdong::Monitoring.includes(:user).where(product: product, disabled: false).where('? <= threshold', current_price)
+    monitorings.each do |monitoring|
+      Rails.logger.info "[JD] #{product.id} - Notify User: #{monitoring.user.id} (threshold: #{monitoring.threshold})"
+      NotificationHelper.send ({
+        user_id: monitoring.user.id,
+        service_id: Jingdong::SERVICE_ID,
+        title: '京东商城',
+        message: "#{product.name}\n当前价格: ¥#{current_price}",
+        push_message: "京东商城:您关注的商品 '#{product.name}' 价格降到 ¥#{current_price} 了 !",
+        url: product.url,
+        web_url: product.web_url,
+        thumb: product.image,
+      })
+      monitoring.update(disabled: true)
     end
     Rails.logger.info "[JD] #{product.id} - Completed"
   end
