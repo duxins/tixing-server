759658b185fc46443ec7702a4a710ba72086f247
duxin <duxins@gmail.com>
2014-12-04 23:47:17 +0800

    Refactor Weibo::User#fetch_weibo_user method

--- app/models/weibo/user.rb
+++ app/models/weibo/user.rb
@@ -1,6 +1,7 @@
-require 'open-uri'
-
 class Weibo::User < ActiveRecord::Base
+  class NotFound < StandardError; end
+  class APIError < StandardError; end
+
   has_many :followers, primary_key:'uid', foreign_key:'uid', class_name: "Weibo::Follower", dependent: :destroy
   scope :available, lambda { where('followers_count >?', 0).order('followers_count DESC') }
 
@@ -17,7 +18,7 @@ class Weibo::User < ActiveRecord::Base
 
       field :name do
         formatted_value do
-          bindings[:view].link_to( "http://www.weibo.com/u/" + bindings[:object].uid.to_s, target: '_blank') do
+          bindings[:view].link_to( bindings[:object].url, target: '_blank') do
             bindings[:view].tag(:img , src: bindings[:object].avatar, width: 20) << " #{value}"
           end
         end
@@ -40,6 +41,7 @@ class Weibo::User < ActiveRecord::Base
     end
   end
 
+
   def followed_by?(user)
     self.followers.where(user: user).present?
   end
@@ -50,10 +52,7 @@ class Weibo::User < ActiveRecord::Base
     url += "?since=#{since}" if since
 
     begin
-      Rails.logger.info("Request #{url}")
-
       feeds = Weibo::User.request_api(url)
-
       return [] if feeds.empty?
 
       Rails.logger.info("Feeds: #{feeds.map{|x| x['mid']}}")
@@ -61,60 +60,62 @@ class Weibo::User < ActiveRecord::Base
       feeds.select!{|feed| feed['mid'] > self.last_weibo_id.to_i}
       feeds.sort_by!{|feed| feed['mid']}
     rescue => e
+      Rails.logger.error("[WEIBO] #{e.message}")
       Rails.logger.error("Weibo API error (feed/#{self.uid}) :#{e.message}")
       return []
     end
   end
 
   def self.request_api(url)
-    json = Timeout::timeout(10) do
+    Rails.logger.info "[WEIBO] Request API: #{url}"
+    json_str = Timeout::timeout(10) do
       Curl::Easy.perform(url) do |curl|
         curl.connect_timeout = 10
       end.body_str
     end
-    JSON.parse(json)
+
+    JSON.parse(json_str)
   end
 
   def self.fetch_weibo_user(name)
+    name.strip!
     url = %[#{ENV['weibo_api']}/user/#{URI.escape(name)}]
-    begin
-      Rails.cache.fetch(self.weibo_user_cache_key(name)) do
-        weibo_user = Weibo::User.request_api(url)
-        self.save_weibo_user(weibo_user)
-      end
-    rescue => e
-      Rails.logger.error("Weibo API error (/user/#{name}): #{e.message}")
+    Rails.logger.info "[WEIBO] Started fetch user: #{name}"
 
-      raise '没有找到该用户' if e.message =~ /404/
-      raise '系统繁忙，请稍后再试'
+    user = self.find_by_name(name)
+    if user.present? and user.updated_at > 5.minutes.ago
+      Rails.logger.info "[WEIBO] User #{name} exists"
+      return user
     end
-  end
 
-  def self.weibo_user_cache_key(name)
-    "service:weibo:user:#{name}"
+    begin
+      weibo_user = self.request_api(url)
+      Rails.logger.info "[WEIBO] Fetched: #{weibo_user.select {|k| ['id', 'name'].include? k}}"
+      self.save_weibo_user weibo_user
+    rescue => e
+      Rails.logger.error "[WEIBO] Error: #{e.message}"
+      raise NotFound if e.message =~ /404/
+      raise APIError.new(e.message)
+    end
   end
 
   def self.save_weibo_user(weibo_user)
-    user = Weibo::User.find_or_initialize_by(uid: weibo_user['id'])
+    user = self.find_or_initialize_by(uid: weibo_user['id'])
     user.name = weibo_user['name']
     user.avatar = weibo_user['avatar']
     user.metadata = weibo_user.to_yaml
-
-    if weibo_user['followers_count'] > 10000
-      user.priority = :high
-    elsif weibo_user['followers_count'] > 1000
-      user.priority = :medium
-    else
-      user.priority = :low
-    end
 
     if user.new_record?
       user.last_weibo_id = weibo_user['mid']
       user.last_checked_at = DateTime.now
     end
     user.save
-    Rails.cache.write(self.weibo_user_cache_key(user.name), user, expires_in:10.minutes)
+    user.touch
     user
+  end
+
+  def url
+    "http://weibo.com/u/#{self.uid}"
   end
 
 end
--- test/models/weibo/user_test.rb
+++ test/models/weibo/user_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class Weibo::UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should fetch weibo user correctly' do
+    VCR.use_cassette('weibo_sina_finance') do
+      user = Weibo::User.fetch_weibo_user('新浪财经')
+      assert_equal 1638782947, user.uid
+      assert_equal '新浪财经', user.name
+      assert_equal 'http://tp4.sinaimg.cn/1638782947/180/5710057679/1', user.avatar
+    end
+  end
+
+  test 'should have correct url attribute' do
+    user = Weibo::User.create(uid: 12345)
+    assert_equal 'http://weibo.com/u/12345', user.url
+  end
+
+  test "should raise NotFound exception when user doesn't exist" do
+    VCR.use_cassette('weibo_sina_non_existent_user') do
+      assert_raise Weibo::User::NotFound do
+        Weibo::User.fetch_weibo_user('non-existent-user')
+      end
+    end
+  end
+
+  test 'should raise APIError error exception when there is a network problem' do
+    stub_request(:get, /localhost/).to_timeout
+    assert_raise Weibo::User::APIError do
+      Weibo::User.fetch_weibo_user('新浪娱乐')
+    end
+  end
+
+  test 'should return cached user' do
+    VCR.use_cassette('weibo_sina_finance') do
+      user = Weibo::User.fetch_weibo_user('新浪财经')
+      assert_equal 1638782947, user.uid
+    end
+
+    stub_request(:get, /localhost/).to_timeout
+    user = Weibo::User.fetch_weibo_user('新浪财经')
+    assert_equal 1638782947, user.uid
+  end
+
 end
