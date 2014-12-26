477a472220a6a1788aabfa47001da3eeb23bb131
duxin <duxins@gmail.com>
2014-12-26 18:18:20 +0800

    Change email format to html

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def new_user
+    UserMailer.new_user_email(User.last)
+  end
 end
